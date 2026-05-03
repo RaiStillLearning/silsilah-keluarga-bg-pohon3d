215bfaba667ad23b30e50f1d7a023bf5a67ae02f
arkanavv <rakhaarkhana98@gmail.com>
2026-05-03 19:30:32 +0700

    feat: add 2016 photo archive section in kegiatan.html

---
+++ scratch_add_kegiatan.js
@@ -0,0 +1,2 @@
+const { createClient } = require('@supabase/supabase-js');
+const supabase = createClient('https://gtuplmzrnggflusstydg.supabase.co', '***'); // I don't have the key here, but I can use the one in data_manager.js if I read it.
